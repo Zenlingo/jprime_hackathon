8b627f52d14a447440fe88f218b37288e82e3ce1
Viktor Moreev <viktor.moreev@zenlingo.com>
2026-06-03 17:00:03 +0300

    initial commit

---
+++ mobile_app/android/app/src/main/kotlin/com/jprime/mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jprime.mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mobile_app_be/src/main/java/jprime/mobile_app_be/MobileAppBeApplication.java
@@ -0,0 +1,13 @@
+package jprime.mobile_app_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppBeApplication.class, args);
+	}
+
+}
