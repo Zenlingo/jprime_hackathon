094aab1bfe35a2cd71e611342ae68a0475de0a40
Dragomila Yasenova <dragomila.yasenova@zenlingo.com>
2026-06-04 15:21:23 +0300

    Update build.gradle.kts

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -14,6 +14,8 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        // Required by flutter_local_notifications.
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -44,4 +46,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Required by flutter_local_notifications when desugaring is enabled.
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
